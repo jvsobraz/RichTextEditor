5d4549727fb0b9f44923ee86698301d7c5ab5169
jvsobraz <jvsobraz@hotmail.com>
2023-07-27 22:28:31 -0300

    Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let optionsButtons = document.querySelectorAll(".option-button");
 let advancedOptionButton = document.querySelectorAll(".adv-option-button");
 let fontName = document.getElementById("fontName");
-let fontSizeRef = document.getElementById("fontSize")
-let writingArea = document.getElementById("text-input")
-let linkButton = document.getElementById("createLink")
-let alignButtons = document.querySelectorAll(".align")
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
 let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
@@ -19,10 +19,10 @@ let fontList = [
     "Cursive",
 ];
 
-const initializer = () => {
+const intializer = () => {
     highlighter(alignButtons, true);
     highlighter(spacingButtons, true);
-    highlighter(formatButtons, true);
+    highlighter(formatButtons, false);
     highlighter(scriptButtons, true);
 
     fontList.map((value) => {
@@ -41,3 +41,56 @@ const initializer = () => {
 
     fontSizeRef.value = 3;
 };
+
+const modifyText = (command, defaultUi, value) => {
+    document.execCommand(command, defaultUi, value);
+};
+
+optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modifyText(button.id, false, null);
+    });
+});
+
+advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+        modifyText(button.id, false, button.value);
+    });
+});
+
+linkButton.addEventListener("click", () => {
+    let userLink = prompt("Enter a URL?");
+    if (/http/i.test(userLink)) {
+        modifyText(linkButton.id, false, userLink);
+    } else {
+        userLink = "http://" + userLink;
+        modifyText(linkButton.id, false, userLink);
+    }
+});
+
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (needsRemoval) {
+                let alreadyActive = false;
+                if (button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+                highlighterRemover(className);
+                if (!alreadyActive) {
+                    button.classList.add("active");
+                }
+            } else {
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active");
+    });
+};
+
+window.onload = intializer();
